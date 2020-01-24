6da08472a0dd620d3a11d08e332659ab7800623d
Lachlan van der Velden <me@lachlanvelden.com>
2020-01-24 11:13:58 +1100

    Inital Commit

---
+++ Models/ServerConfiguration.cs
@@ -0,0 +1,9 @@
+namespace IdentityServer.Models
+{
+    public class ServerConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
